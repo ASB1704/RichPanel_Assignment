5908f2822c724ce9570b78f3e90f217c5786fe9b
ASB1704 <singhanurag17042002@gmail.com>
2023-08-27 03:30:10 +0530

    minor updates in ui

--- FrontEnd/src/Components/Selected_plan/Selected_Plan_Screen.jsx
+++ FrontEnd/src/Components/Selected_plan/Selected_Plan_Screen.jsx
@@ -1,50 +1,52 @@
-import './Selected_plan_Screen.css'
+import "./Selected_plan_Screen.css";
 import { useGlobalContext } from "../../StateContext";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 export const Selected_plan_Screen = () => {
   const navigate = useNavigate();
-  const {user,setuser} = useGlobalContext();
+  const { user, setuser } = useGlobalContext();
 
-  const devices = user.plan.devices;
-  const formattedDevices = devices.join(" + ");
+  const devices = user?.plan.devices;
+  const formattedDevices = devices?.join(" + ");
 
-  const price = user.plan.price;
-  const cycle = user.plan.cycle;
+  const price = user?.plan.price;
+  const cycle = user?.plan.cycle;
   const cycleText = cycle === "monthly" ? "/mo" : "/yr";
 
-  const subscriptionStartDate = new Date(user.plan.dateofsubscription);
-  const planCycle = user.plan.cycle;
+  const subscriptionStartDate = new Date(user?.plan.dateofsubscription);
+  const planCycle = user?.plan.cycle;
   const handleCancelPlan = async () => {
     try {
-        const updatedUser = {
-            ...user,
-            plan: {
-                cycle: null,
-                name: null,
-                price: null,
-                devices: [],
-                state: 'cancelled',
-                dateofsubscription: null
-            }
-        };
+      const updatedUser = {
+        ...user,
+        plan: {
+          cycle: null,
+          name: null,
+          price: null,
+          devices: [],
+          state: "cancelled",
+          dateofsubscription: null,
+        },
+      };
 
-        const { data } = await axios.put(`${import.meta.env.VITE_BACKEND_URI}/UpdateUser`, updatedUser);
-        console.log(data, "asdhfosd");
+      const { data } = await axios.put(
+        `${import.meta.env.VITE_BACKEND_URI}/UpdateUser`,
+        updatedUser
+      );
+      console.log(data, "asdhfosd");
 
-        setuser({
-          ...user,
-          plan: {
-              ...user.plan,
-              state: "cancelled"
-          }
-        });
+      setuser({
+        ...user,
+        plan: {
+          ...user?.plan,
+          state: "cancelled",
+        },
+      });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-};
-  
-  
+  };
+
   const renewalDate = new Date(subscriptionStartDate);
   if (planCycle === "monthly") {
     renewalDate.setMonth(renewalDate.getMonth() + 1);
@@ -54,7 +56,7 @@ export const Selected_plan_Screen = () => {
   renewalDate.setDate(renewalDate.getDate() + 1);
 
   function formatDate(date) {
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const options = { year: "numeric", month: "short", day: "numeric" };
     const formattedDate = date.toLocaleDateString(undefined, options);
 
     const day = date.getDate();
@@ -65,17 +67,17 @@ export const Selected_plan_Screen = () => {
 
   function getDaySuffix(day) {
     if (day >= 11 && day <= 13) {
-      return 'th';
+      return "th";
     }
     switch (day % 10) {
       case 1:
-        return 'st';
+        return "st";
       case 2:
-        return 'nd';
+        return "nd";
       case 3:
-        return 'rd';
+        return "rd";
       default:
-        return 'th';
+        return "th";
     }
   }
 
@@ -83,42 +85,33 @@ export const Selected_plan_Screen = () => {
   const formattedRenewalDate = formatDate(renewalDate);
 
   return (
-    <div className='CurrentMain'>
+    <div className="CurrentMain">
       <div className="CurretnContainer">
         <div className="CurretnContainer_item1">
           <div className="CurretnContainer_item_in1">
-            <div className='CurretnContainer_item_in11'>
+            <div className="CurretnContainer_item_in11">
               Current Plan Details
             </div>
-            {user.plan.state === "active" ?
-              <div className='CurretnContainer_item_in12'>
-                Active
-              </div> :
-              <div className='CurretnContainer_item_in122'>
-                Cancelled
-              </div>}
 
-            <div className='CurretnContainer_item_in13'>
+            {user?.plan.state === "active" ? (
+              <div className="CurretnContainer_item_in12">Active</div>
+            ) : (
+              <div className="CurretnContainer_item_in122">Cancelled</div>
+            )}
 
-            </div>
-          </div >
+            <div className="CurretnContainer_item_in13"></div>
+          </div>
           <div className="CurretnContainer_item_in2" onClick={handleCancelPlan}>
-            {user.plan.state === "active" ?
-              <div className=''>
-                Cancel
-              </div> :
-              <div className=''>
-                
-              </div>}
+            {user?.plan.state === "active" ? (
+              <div className="">Cancel</div>
+            ) : (
+              <div className=""></div>
+            )}
           </div>
         </div>
         <div className="CurretnContainer_item2">
-          <div className='CurretnContainer_item2_in1'>
-            {user.plan.name}
-          </div>
-          <div className="CurretnContainer_item2_in2">
-            {formattedDevices}
-          </div>
+          <div className="CurretnContainer_item2_in1">{user?.plan.name}</div>
+          <div className="CurretnContainer_item2_in2">{formattedDevices}</div>
         </div>
         <div className="CurretnContainer_item3">
           {price && (
@@ -129,16 +122,19 @@ export const Selected_plan_Screen = () => {
           )}
         </div>
         <div className="CurretnContainer_item4">
-          <button className="CurretnContainer_item4_button" onClick={() => navigate("/Subscription_Model")}>
-            {user.plan.state === "active" ? "Change Plan" : "Choose Plan"}
+          <button
+            className="CurretnContainer_item4_button"
+            onClick={() => navigate("/Subscription_Model")}
+          >
+            {user?.plan.state === "active" ? "Change Plan" : "Choose Plan"}
           </button>
         </div>
         <div className="CurretnContainer_item5">
-          {user.plan.state === "active" ? `Your subsciption has started from ${formattedStartDate} and will auto renew on ${formattedRenewalDate}.` : `Your subsciption was cancelled and you will loose access to services on ${formattedStartDate}.`}
-
+          {user?.plan.state === "active"
+            ? `Your subsciption has started from ${formattedStartDate} and will auto renew on ${formattedRenewalDate}.`
+            : `Your subsciption was cancelled and you will loose access to services on ${formattedStartDate}.`}
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
